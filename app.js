aeea0c99ca1de04f1ea712bb4a34d508a4a6769e
AtharvaKarkar <93367238+AtharvaKarkar@users.noreply.github.com>
2023-04-14 14:58:46 +0530

    js file added

---
+++ app.js
@@ -0,0 +1,80 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+    {
+      id: 1,
+      title: "Air Force",
+      price: 119,
+      colors: [
+        {
+          code: "black",
+          img: "./Image/air.png",
+        },
+        {
+          code: "darkblue",
+          img: "./Image/air2.png",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Air Jordan",
+      price: 149,
+      colors: [
+        {
+          code: "lightgray",
+          img: "./Image/jordan.png",
+        },
+        {
+          code: "green",
+          img: "./Image/jorden2.png",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Blazer",
+      price: 109,
+      colors: [
+        {
+          code: "lightgray",
+          img: "./Image/blazer.png",
+        },
+        {
+          code: "green",
+          img: "./Image/blazer2.png",
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Crater",
+      price: 129,
+      colors: [
+        {
+          code: "black",
+          img: "./Image/crater.png",
+        },
+        {
+          code: "lightgray",
+          img: "./Image/crater2.png",
+        },
+      ],
+    },
+    {
+      id: 5,
+      title: "Hippie",
+      price: 99,
+      colors: [
+        {
+          code: "gray",
+          img: "./Image/hippie.png",
+        },
+        {
+          code: "black",
+          img: "./Image/hippie2.png",
+        },
+      ],
+    },
+  ];
